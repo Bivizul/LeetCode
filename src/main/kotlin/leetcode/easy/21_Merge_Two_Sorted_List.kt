bc998e7c4d5d2d61f86669a3fdbeecd3f531c472
Vladislav Belyaev <mamochapa@gmail.com>
2023-06-17 10:49:01 +0200

    26 complete

---
+++ src/main/kotlin/leetcode/easy/21_Merge_Two_Sorted_List.kt
@@ -0,0 +1,21 @@
+package leetcode.easy
+
+/**
+ * You are given the heads of two sorted linked lists list1 and list2.
+ *
+ * Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+ *
+ * Return the head of the merged linked list.
+ */
+
+fun main() {
+
+}
+
+//fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+//
+//}
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
